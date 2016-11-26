2127a6a164f5b6a56e413400ac57fe75ed91b7fd
An7ar35 <eadavison@protonmail.com>
2016-11-26 10:52:09 +0100

    [sbp::io::DotExport] Functionalities implemented

--- src/io/DotExport.cpp
+++ src/io/DotExport.cpp
@@ -1,1 +1,52 @@
 #include "DotExport.h"
+
+/**
+ * Constructor
+ * @param writer EADLib File writer
+ */
+sbp::io::DotExport::DotExport( eadlib::io::FileWriter &writer ) :
+    _writer( writer )
+{}
+
+/**
+ * Destructor
+ */
+sbp::io::DotExport::~DotExport() {}
+
+/**
+ * Exports the graph as a Dot file
+ * @param graph        Graph to export
+ * @param weight_label Shows multi-edges as weight labels (default=true)
+ * @return Success
+ */
+bool sbp::io::DotExport::exportToDot( const eadlib::WeightedGraph<std::string> &graph, const bool &weight_label ) {
+    if( !_writer.isOpen() && !_writer.open() ) {
+        LOG_ERROR( "[sbp::io::DotExport::export(..)] Could not open file '", _writer.getFileName(), "'." );
+        return false;
+    }
+    _writer.write( "digraph genome {\n" );
+    _writer.write( "\tnode [shape = circle]\n" );
+    for( auto node : graph ) {
+        if( node.second.childrenList.empty() ) {
+            _writer.write( "\t" + node.first );
+        } else {
+            for( auto dest : node.second.childrenList ) {
+                if( weight_label ) { //shows single edges with weight
+                    _writer.write( "\t"
+                                   + node.first
+                                   + " -> "
+                                   + dest
+                                   + " [label=\""
+                                   + std::to_string( node.second.weight.at( dest ) )
+                                   + "\"]\n" );
+                } else { //shows every duplicate edges
+                    for( size_t w  = 0; w < node.second.weight.at( dest ); w++ ) {
+                        _writer.write( "\t" + node.first + " -> " + dest + "\n" );
+                    }
+                }
+            }
+        }
+    }
+    _writer.write( "}" );
+    return true;
+}
--- src/io/DotExport.h
+++ src/io/DotExport.h
@@ -1,10 +1,21 @@
 #ifndef SUPERBUBBLE_PERFORMANCE_DOTEXPORT_H
 #define SUPERBUBBLE_PERFORMANCE_DOTEXPORT_H
 
-namespace sb {
+#include "eadlib/io/FileWriter.h"
+#include "eadlib/logger/Logger.h"
+#include "eadlib/datastructure/WeightedGraph.h"
+
+namespace sbp {
     namespace io {
         class DotExport {
-
+          public:
+            enum class EdgeType { MULTI_EDGE, WEIGHT_LABEL};
+            DotExport( eadlib::io::FileWriter &writer );
+            ~DotExport();
+            bool exportToDot( const eadlib::WeightedGraph<std::string> &graph,
+                              const bool &weight_label = true );
+          private:
+            eadlib::io::FileWriter &_writer;
         };
     }
 }
