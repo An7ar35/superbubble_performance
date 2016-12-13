990c240a7d996e39dc5e02bc77b0e26422d15661
An7ar35 <eadavison@protonmail.com>
2016-12-13 18:43:25 +0100

    [spb::io::DotExport] templated export function to work on various WeightedGraph<T>

--- src/io/DotExport.h
+++ src/io/DotExport.h
@@ -12,12 +12,75 @@ namespace sbp {
             enum class EdgeType { MULTI_EDGE, WEIGHT_LABEL};
             DotExport( eadlib::io::FileWriter &writer );
             ~DotExport();
-            bool exportToDot( const std::string &graph_name,
-                              const eadlib::WeightedGraph<std::string> &graph,
-                              const bool &weight_label );
+            template<class T> bool exportToDot( const std::string &graph_name,
+                                                const eadlib::WeightedGraph<T> &graph,
+                                                const bool &weight_label );
           private:
             eadlib::io::FileWriter &_writer;
         };
+
+        //-----------------------------------------------------------------------------------------------------------------
+        // DotExport class public method implementations
+        //-----------------------------------------------------------------------------------------------------------------
+        /**
+         * Constructor
+         * @param writer EADLib File writer
+         */
+        DotExport::DotExport( eadlib::io::FileWriter &writer ) :
+            _writer( writer )
+        {}
+
+        /**
+         * Destructor
+         */
+        DotExport::~DotExport() {}
+
+
+        /**
+         * Exports the graph as a Dot file
+         * @param graph        Graph to export
+         * @param weight_label Shows multi-edges as weight labels (default=true)
+         * @return Success
+         */
+        template<class T> bool DotExport::exportToDot( const std::string &graph_name,
+                                                       const eadlib::WeightedGraph<T> &graph,
+                                                       const bool &weight_label ) {
+            if( !_writer.isOpen() && !_writer.open() ) {
+                LOG_ERROR( "[sbp::io::DotExport::exportToDot(..)] Could not open file '", _writer.getFileName(), "'." );
+                return false;
+            }
+            _writer.write( "digraph " + graph_name + " {\n" );
+            _writer.write( "\tnode [shape = circle]\n" );
+            for( auto node : graph ) {
+                if( node.second.childrenList.empty() && node.second.parentsList.empty() ) {
+                    std::ostringstream oss;
+                    oss << "\t" << node.first << ";\n";
+                    _writer.write( oss.str() );
+                } else {
+                    for( auto dest : node.second.childrenList ) {
+                        if( weight_label ) { //shows single edges with weight
+                            std::ostringstream oss;
+                            oss << "\t"
+                                << node.first
+                                << " -> "
+                                << dest
+                                << " [label=\""
+                                <<  std::to_string( node.second.weight.at( dest ) )
+                                << "\"]\n";
+                            _writer.write( oss.str() );
+                        } else { //shows every duplicate edges
+                            for( size_t w  = 0; w < node.second.weight.at( dest ); w++ ) {
+                                std::ostringstream oss;
+                                oss << "\t" << node.first << " -> " << dest << "\n";
+                                _writer.write( oss.str() );
+                            }
+                        }
+                    }
+                }
+            }
+            _writer.write( "}" );
+            return true;
+        }
     }
 }
 
