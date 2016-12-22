f6f0b8c29347eba1e04069732a26539f3c59074c
An7ar35 <eadavison@protonmail.com>
2016-12-23 00:32:02 +0100

    [spb::algo::SB_Linear] Added concatenating method for the single SCCs found with sbp::algo::Tarjan

--- src/algorithm/superbubble/SB_Linear.cpp
+++ src/algorithm/superbubble/SB_Linear.cpp
@@ -17,16 +17,45 @@ sbp::algo::SB_Linear::~SB_Linear() {}
  * Run the detection algorithm
  * @return Number of Superbubble detected
  */
-size_t sbp::algo::SB_Linear::run() {
+bool sbp::algo::SB_Linear::run( std::list<container::SuperBubble> &superbubble_list ) {
     //TODO
-    auto tarjan = std::make_unique<Tarjan>( Tarjan( _graph ) );
-    for( auto list : *tarjan ) {
-        for( auto e : list ) {
-            std::cout << e << ",";
+    auto strongly_connected_components = std::make_unique<Tarjan>( Tarjan( _graph ) );
+    auto singletonSCC_count            = concatenateSingletonSCCs( *strongly_connected_components );
+    auto subGraphs_count               = strongly_connected_components->size();
+    auto subGraphs                     = std::vector<std::unique_ptr<eadlib::Graph<size_t>>>();
+    auto global2local_id_index         = std::make_unique<std::vector<size_t>>( _graph.nodeCount() );
+
+    std::cout << "Found " << singletonSCC_count << " singleton SCCs." << std::endl;
+    for( auto scc : *strongly_connected_components ) {
+        for( auto vertex_id : scc ) {
+            std::cout << vertex_id << ",";
         }
         std::cout << std::endl;
     }
 
     return 0;
+}
+
+/**
+ * Concatenate all singletons sccs found in the graph into the first list of SCCs in the Tarjan instance
+ * @param sccs Tarjan instance
+ * @return Number of singletons found
+ */
+size_t sbp::algo::SB_Linear::concatenateSingletonSCCs( sbp::algo::Tarjan &sccs ) {
+    size_t count { 0 };
+    auto first = sccs.begin();
+    auto next  = sccs.begin();
+    if( first != sccs.end() && sccs.size() > 1 ) {
+        if( first->size() == 1 ) {
+            next++;
+            count++;
+            while( next != sccs.end() && next->size() == 1 ) {
+                first->emplace_back( next->front() );
+                next = sccs.erase( next );
+                count++;
+            }
+        }
+    }
+    return count;
 }
 
--- src/algorithm/superbubble/SB_Linear.h
+++ src/algorithm/superbubble/SB_Linear.h
@@ -1,8 +1,23 @@
+/**
+    @class          sbp::algo::SB_Linear
+    @brief          Implementation of the O(n + m) superbubble algorithm
+
+                    //TODO cite here!!
+
+    @dependencies   eadlib::WeightedGraph<T>, eadlib::Graph<T>,
+                    sbp::algo::container::SuperBubble, sbp::algo::Tarjan
+
+    @author         E. A. Davison
+    @copyright      E. A. Davison 2016
+    @license        GNUv2 Public License
+**/
 #ifndef SUPERBUBBLE_PERFORMANCE_SB_LINEAR_H
 #define SUPERBUBBLE_PERFORMANCE_SB_LINEAR_H
 
+#include <eadlib/datastructure/Graph.h>
 #include <eadlib/datastructure/WeightedGraph.h>
 
+#include "container/SuperBubble.h"
 #include "../Tarjan.h"
 
 namespace sbp {
@@ -11,8 +26,9 @@ namespace sbp {
           public:
             SB_Linear( const eadlib::WeightedGraph<size_t> &graph );
             ~SB_Linear();
-            size_t run();
+            bool run( std::list<container::SuperBubble> &superbubble_list );
           private:
+            size_t concatenateSingletonSCCs( sbp::algo::Tarjan &sccs );
             const eadlib::WeightedGraph<size_t> _graph;
         };
     }
