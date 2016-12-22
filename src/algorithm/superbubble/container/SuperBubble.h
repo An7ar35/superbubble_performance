f37b60a9bc59c5a2ea8e528ece3d89ae4555457e
An7ar35 <eadavison@protonmail.com>
2016-12-23 00:30:51 +0100

    [spb::algo::container::SuperBubble] Added struct for describing superbubble in graph

---
+++ src/algorithm/superbubble/container/SuperBubble.h
@@ -0,0 +1,15 @@
+#ifndef SUPERBUBBLE_PERFORMANCE_SUPERBUBBLE_H
+#define SUPERBUBBLE_PERFORMANCE_SUPERBUBBLE_H
+
+namespace sbp {
+    namespace algo {
+        namespace container {
+            struct SuperBubble {
+                size_t _in_id;
+                size_t _out_id;
+            };
+        }
+    }
+}
+
+#endif //SUPERBUBBLE_PERFORMANCE_SUPERBUBBLE_H
