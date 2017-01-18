201212b295a65fee6aae4f3dfb336f24a933806e
An7ar35 <eadavison@protonmail.com>
2017-01-18 23:55:33 +0100

    [spb::algo::SB_QLinear] added graph partitioning calls

--- src/algorithm/superbubble/SB_QLinear.cpp
+++ src/algorithm/superbubble/SB_QLinear.cpp
@@ -1,4 +1,5 @@
 #include "SB_QLinear.h"
+#include "../../graph/SubGraph.h"
 
 /**
  * Constructor
@@ -19,9 +20,17 @@ sbp::algo::SB_QLinear::~SB_QLinear() {}
  * @return Success
  */
 bool sbp::algo::SB_QLinear::run( std::list<sbp::algo::container::SuperBubble> &superbubble_list ) {
+    //Find SCCs
     auto strongly_connected_components = Tarjan( _graph ).findSCCs();
-    Tarjan::concatenateSingletonSCCs( *strongly_connected_components );
+    auto singleton_count = Tarjan::concatenateSingletonSCCs( *strongly_connected_components );
 
+    //Partition graph into sub-graphs
+    std::list<std::unique_ptr<graph::SubGraph>> sub_graphs;
+    size_t sg_count { 0 };
+    for( auto it = strongly_connected_components->begin(); it != strongly_connected_components->end(); ++it) {
+        sub_graphs.emplace_back( std::make_unique<graph::SubGraph>( _graph, *it, std::string( "SubGraph" + sg_count ) ) );
+        sg_count++;
+    }
 
 
 
